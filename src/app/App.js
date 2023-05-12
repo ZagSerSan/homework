1df296d7e57b522c1fb7e97648ea08cd4d5cbefa
ZagSerSan <yan.steys1997@gmail.com>
2023-05-12 15:26:46 +0200

    10. Переадресация (редирект)

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,11 @@
-import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min"
-
+import { Route, Switch, Redirect } from "react-router-dom/cjs/react-router-dom.min"
 // my components
 import NavBar from "./components/navBar"
 import Home from "./components/home"
 import Dashboard from "./components/dashboard"
 import Login from "./components/login"
 import Posts from "./components/posts"
+import NotFound from "./components/notFound"
 
 function App() {
   return (
@@ -17,6 +17,9 @@ function App() {
         <Route path="/dashboard" component={Dashboard}/>
         <Route path="/login" component={Login} />
         <Route path="/posts/:postId?" component={Posts}/>
+        <Route path="/404" component={NotFound}/>
+        <Redirect from='/admin' to='/dashboard'/>
+        <Redirect to='/404'/>
       </Switch>
     </div>
   )
