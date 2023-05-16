33537be694bf2e146541b618b834f22c37ab8577
ZagSerSan <yan.steys1997@gmail.com>
2023-05-16 08:29:35 +0200

    useHistory and useParams

--- src/app/components/post.jsx
+++ src/app/components/post.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-const Post = ({post}) => {
+const Post = ({post, goToList}) => {
   
-  return <h3>{post ? post.label : `Post is not found`}</h3>
+  return <>
+    <h3>{post ? post.label : `Post is not found`}</h3>
+    <button
+      onClick={()=>goToList(post)}
+      style={{cursor: 'pointer'}}
+    >
+      back to list
+    </button>
+  </>
 }
  
 export default Post
--- src/app/components/posts.jsx
+++ src/app/components/posts.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import query from 'query-string'
 import _ from 'lodash'
 // components
@@ -6,12 +8,14 @@ import PostList from "./postList"
 import Post from "./post"
 
 const Posts = ({match, location}) => {
+  const history = useHistory()
+  const params = useParams()
   const posts = [
     {id: 1, title: 'Post 1', label: 'It`s the post 1'},
     {id: 2, title: 'Post 2', label: 'It`s the post 2'},
     {id: 3, title: 'Post 3', label: 'It`s the post 3'}
   ]
-  const postId = match.params.postId
+  const {postId} = params
   const getPostById = (postId) => {
     return posts.find( post => (post.id.toString() === postId))
   }
@@ -21,8 +25,21 @@ const Posts = ({match, location}) => {
   const cropPosts = search.count
     ? _(posts).slice(0).take(search[Object.keys(search)]).value()
     : posts
+  
+  // Создаём функцию для перехода к списку постов
+  const goToList = (hasPost) => {
+      // Вызываем метод истории
+      hasPost ? history.push("/posts") : history.replace("/posts")
+  }
 
-  return <>{postId ? <Post post={getPostById(postId)}/> : <PostList posts={cropPosts}/>}</>
+  return (
+    <>
+      {postId
+        ? <Post post={getPostById(postId)} goToList={goToList}/>
+        : <PostList posts={cropPosts}/>
+      }
+    </>
+  )
 }
  
 export default Posts
