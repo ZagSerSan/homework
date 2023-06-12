7ca81bba73cd13bd9f07e4174a7fea51e22f376c
ZagSerSan <yan.steys1997@gmail.com>
2023-06-12 14:55:47 +0200

    prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,8 @@
+// файл, в котором настраиваем как будет форматировать код
+// команда npx prettier --write .
 module.exports = {
-    trailingComma: "none",
-    tabWidth: 4,
-    semi: true
-};
+  trailingComma: 'none',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true
+}
--- src/app/components/examples/useRef/programmableActions.jsx
+++ src/app/components/examples/useRef/programmableActions.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import CardWrapper from "../../common/Card";
-import SmallTitle from "../../common/typografy/smallTitle";
+import React from 'react'
+import CardWrapper from '../../common/Card'
+import SmallTitle from '../../common/typografy/smallTitle'
 const ProgrammableActionsExample = () => {
-    return (
-        <CardWrapper>
-            <SmallTitle className="card-title">
-                Программируемые действия и свойства
-            </SmallTitle>
-        </CardWrapper>
-    );
-};
+  return (
+    <CardWrapper>
+      <SmallTitle className="card-title">
+        Программируемые действия и свойства
+      </SmallTitle>
+    </CardWrapper>
+  )
+}
 
-export default ProgrammableActionsExample;
+export default ProgrammableActionsExample
