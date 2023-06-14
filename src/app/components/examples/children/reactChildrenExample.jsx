994e4bfba59104d5f952001fc9109924e35b4fbe
ZagSerSan <yan.steys1997@gmail.com>
2023-06-14 12:49:33 +0200

    cloneElementExample

--- src/app/components/examples/children/reactChildrenExample.jsx
+++ src/app/components/examples/children/reactChildrenExample.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import CardWrapper from '../../common/Card'
 import SmallTitle from '../../common/typografy/smallTitle'
+import Divider from '../../common/divider'
+import TextField from '../../common/form/textField'
+
+const FormComponent = ({children}) => {
+  // const [data, setData] = useState()
+  // const handleChange = (target) => {
+  //   setData(prevState => ({...prevState, [target.name]: target.value}))
+  // }
+  return React.Children.map(children, (child) => {
+    // console.log('child', child)
+    return child
+  })
+}
+FormComponent.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ])
+}
+
 const ReactChildrenExample = () => {
   return (
     <CardWrapper>
       <SmallTitle>Clone form and add props</SmallTitle>
+      <Divider/>
+      <FormComponent>
+        <TextField name='email' label='email'/>
+        <TextField name='password' label='password'/>
+      </FormComponent>
     </CardWrapper>
   )
 }
