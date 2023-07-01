e48929791331997c43a0783b9b649f399eddb8da
ZagSerSan <yan.steys1997@gmail.com>
2023-07-01 10:20:24 +0200

    9. Глобальное отлавливание неожиданных ошибок

--- qualities/src/app/pages/editQuality.jsx
+++ qualities/src/app/pages/editQuality.jsx
@@ -3,10 +3,24 @@ import EditForm from '../components/ui/editForm'
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min'
 import axios from 'axios'
 
+axios.interceptors.response.use((res) => res, function(error) {
+  // условие для отлавливания ожидаемой ошибки (см коды статусов http)
+  const expectedErrors = 
+  error.response &&
+  error.response.status >= 400 &&
+  error.response.status < 500
+  if (!expectedErrors) {
+    // неожидаемые ошибки
+    console.log('Unexpected Errors')
+  }
+  return Promise.reject(error)
+})
+
 const EditQualityPage = () => {
   const [quality, setQuality] = useState(null)
   const id = useParams().id
   const qualityEndPoint = `http://localhost:4000/api/v1/quality/${id}`
+
   useEffect(async () => {
     const {data} = await axios.get(qualityEndPoint)
     setQuality(data.content)
@@ -18,18 +32,8 @@ const EditQualityPage = () => {
         .put(qualityEndPoint + 'dasd', form)
         .then(respons => console.log(respons.data.content))
     } catch (error) {
-      // условие для отлавливания ожидаемой ошибки (см коды статусов http)
-      const expectedErrors = 
-        error.response &&
-        error.response.status >= 400 &&
-        error.response.status < 500
-      if (!expectedErrors) {
-        // неожидаемые ошибки
-        console.log('Unexpected Errors')
-      } else {
-        // ожидаемые ошибки, то есть не входят в условие ожидаемых
-        console.log('Expected Errors')
-      }
+      // ожидаемые ошибки, то есть не входят в условие ожидаемых
+      console.log('Expected Errors')
     }
   }
 
