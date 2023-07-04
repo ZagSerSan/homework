c29e490fd2f5903a3b6ba4eb959b38052faf4128
ZagSerSan <yan.steys1997@gmail.com>
2023-07-04 10:42:20 +0200

    useForm(data, onSubmit)

--- qualities/src/app/components/ui/qualityForm.jsx
+++ qualities/src/app/components/ui/qualityForm.jsx
@@ -1,29 +1,11 @@
-import React, {useState} from 'react'
+import React, { useContext } from 'react'
 import SelectField from '../common/form/selectField'
 import TextField from '../common/form/textField'
 import colors from '../../constants/colors.json'
-// import useForm from '../../hooks/useForm'
+import useForm from '../../hooks/useForm'
 // import { QualitiesContex } from '../../App'
 
-const useForm = (initialState = {}, onSubmit) => {
-  const [form, setForm] = useState(initialState)
-  console.log('initialState :>> ', initialState);
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    onSubmit?.(form)
-  }
-  const handleChange = (target) => {
-    setForm((prevState) => ({
-      ...prevState,
-      [target.name]: target.value
-    }))
-  }
-  return { form, handleChange, handleSubmit }
-}
-
 const QualityForm = ({data, onSubmit}) => {
-  // const { form, handleChange, handleSubmit } = useForm({}, onSubmit)
   const { form, handleChange, handleSubmit } = useForm(data, onSubmit)
   // const data = useContext(QualitiesContex)
 
