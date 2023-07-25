db56740f9c2b7d8a176af7ddc6549e545b92c01c
ZagSerSan <yan.steys1997@gmail.com>
2023-07-25 10:10:42 +0200

    6. Композиция функций

--- react-redux-app/src/App.jsx
+++ react-redux-app/src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css'
+import {compose, pipe} from 'lodash/fp'
 
 const App = () => {
+  const x = 2
+  const double = (num) => num * 2
+  const square = (num) => num * num
+  const half = (num) => num / 2
+
+  const mathCalculate = compose (half, square, double)
 
   return (
-    <h1>app</h1>
+    <h1>{mathCalculate(x)}</h1>
+    // <h1>app</h1>
   )
 }
 
