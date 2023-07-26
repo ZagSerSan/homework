db7dffc5e5296dbc8838b215caf81c8e2a1bdcb8
ZagSerSan <yan.steys1997@gmail.com>
2023-07-26 07:52:37 +0200

    12. Разработка Redux с нуля. Dispatch

--- react-redux-app/src/App.jsx
+++ react-redux-app/src/App.jsx
@@ -6,15 +6,31 @@ function createStore(initialState) {
   function getState() {
     return state
   }
-  return {getState}
+  function dispatch(action) {
+    console.log(action)
+    if (action === 'task/completed') {
+      const newArray = [...state]
+      const elementIndex = newArray.findIndex(el => el.id === action.payload.id)
+      newArray[elementIndex].completed = true
+      state = newArray
+      console.log(state)
+    }
+  }
+  return { getState, dispatch }
 }
 
-const store = createStore([{id: 1, description: 'Task 1'}])
+const store = createStore([{id: 1, description: 'Task 1', completed: false}])
 
 const App = () => {
   console.log(store.getState())
+  
   return (
-    <h1>app</h1>
+    <>
+      <h1>app</h1>
+      <button onClick={() => store.dispatch({ type: 'task/completed', payload: {id: 1}})}>
+        Complete
+      </button>
+    </>
   )
 }
 
