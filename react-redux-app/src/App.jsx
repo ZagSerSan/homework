9d59694a9f651d3bf042ed07f3d94449f2ed4b80
ZagSerSan <yan.steys1997@gmail.com>
2023-07-27 17:11:38 +0200

    15. Разработка Redux с нуля. Observer Pattern

--- react-redux-app/src/App.jsx
+++ react-redux-app/src/App.jsx
@@ -1,35 +1,69 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function createStore(initialState) {
+function taskReducer(state, action) {
+  switch (action.type) {
+    case 'task/completed':
+      const newArray = [...state]
+      const elementIndex = newArray.findIndex(el => el.id === action.payload.id)
+      newArray[elementIndex].completed = !newArray[elementIndex].completed
+      return newArray
+      break;
+  
+    default:
+      break;
+  }
+}
+function createStore(reducer, initialState) {
   let state = initialState
+  let listeners = []
 
   function getState() {
     return state
   }
   function dispatch(action) {
-    console.log(action)
-    if (action === 'task/completed') {
-      const newArray = [...state]
-      const elementIndex = newArray.findIndex(el => el.id === action.payload.id)
-      newArray[elementIndex].completed = true
-      state = newArray
-      console.log(state)
+    state = reducer(state, action)
+    for (let i = 0; i < listeners.length; i++) {
+      const listener = listeners[i]
+      listener()
     }
   }
-  return { getState, dispatch }
+  // функция добавления слушателей (в listeners)
+  function subscribe(listener) {
+    listeners.push(listener)
+  }
+  return { getState, dispatch, subscribe }
+}
+const store = createStore(taskReducer, [
+  {id: 1, description: 'Task 1', completed: false},
+  {id: 2, description: 'Task 2', completed: false}
+])
+const completeTask = (taskId) => {
+  store.dispatch({ type: 'task/completed', payload: {id: taskId}})
 }
 
-const store = createStore([{id: 1, description: 'Task 1', completed: false}])
-
+// component
 const App = () => {
-  console.log(store.getState())
+  const [state, setState] = useState(store.getState())
+  // const state = store.getState()
   
+    useEffect(() => {
+    store.subscribe(() => {
+      setState(store.getState())
+    })
+  }, [])
+
   return (
     <>
       <h1>app</h1>
-      <button onClick={() => store.dispatch({ type: 'task/completed', payload: {id: 1}})}>
-        Complete
-      </button>
+      <ul>
+        {state.map(task => <li key={task.id}>
+          <p>{task.description}</p>
+          <p>{`Completed: ${task.completed}`}</p>
+          <button onClick={()=>completeTask(task.id)}>Complete</button>
+          <hr/>
+        </li>)}
+      </ul>
     </>
   )
 }
