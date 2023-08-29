6a8df83b0e31da8b296eb3ff4bcbaae4053385d7
ZagSerSan <yan.steys1997@gmail.com>
2023-08-29 09:10:35 +0200

    5. Routes и Route. Небольшие корректировки

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { isLoggedInSelector } from "../store/authSlice";
 function ProtectedRoute({ component: Component, children, ...rest }) {
@@ -10,16 +10,16 @@ function ProtectedRoute({ component: Component, children, ...rest }) {
             {...rest}
             render={(props) => {
                 if (!isLoggedIn) {
-                    return (
-                        <Redirect
-                            to={{
-                                pathname: "/auth/login",
-                                state: {
-                                    referrer: props.location,
-                                },
-                            }}
-                        />
-                    );
+                    // return (
+                        // <Redirect
+                        //     to={{
+                        //         pathname: "/auth/login",
+                        //         state: {
+                        //             referrer: props.location,
+                        //         },
+                        //     }}
+                        // />
+                    // );
                 }
                 return Component ? <Component {...props} /> : children;
             }}
