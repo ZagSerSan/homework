1af0a275ae242ff981b4b41b17ed84b10d1f68c0
ZagSerSan <yan.steys1997@gmail.com>
2023-08-30 21:12:10 +0200

    12. useRoutes()

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,59 @@
 import React from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
+// import { Navigate, Route, Routes, useRoutes } from 'react-router-dom'
+import { useLocation, useRoutes } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
-import AuthLayout from './layouts/AuthLayout'
-import PostsLayout from './layouts/PostsLayout'
-import MainPage from './pages/MainPage'
-import NavBar from './components/NavBar/NavBar'
-// import ProtectedRoute from "./components/ProtectedRoute";
 import withRedux from './hoc/withRedux'
 import withRouter from './hoc/withRouter'
 import 'react-toastify/dist/ReactToastify.css'
-import PostsListPage from './pages/Posts/PostsListPage'
-import PostPage from './pages/Posts/PostPage'
-import LoginPage from './pages/LoginPage'
-import SignUpPage from './pages/SigupPage'
-import ProtectedRoute from './components/ProtectedRoute'
-// import { useSelector } from 'react-redux'
-// import { isLoggedInSelector } from './store/authSlice'
+import NavBar from './components/NavBar/NavBar'
+// import AuthLayout from './layouts/AuthLayout'
+// import PostsLayout from './layouts/PostsLayout'
+// import MainPage from './pages/MainPage'
+// import PostsListPage from './pages/Posts/PostsListPage'
+// import PostPage from './pages/Posts/PostPage'
+// import LoginPage from './pages/LoginPage'
+// import SignUpPage from './pages/SigupPage'
+// import ProtectedRoute from './components/ProtectedRoute'
+import routes from './routes'
+import { useSelector } from 'react-redux'
+import { isLoggedInSelector } from './store/authSlice'
 
 function App() {
-  // const isLoggedIn = useSelector(isLoggedInSelector())
+  const isLoggedIn = useSelector(isLoggedInSelector())
+  const location = useLocation()
+  const elements = useRoutes(routes(isLoggedIn, location))
+
   return (
     <div className="min-h-screen bg-slate-50 dark:bg-slate-900 transition-colors duration-150 flex flex-col">
       <NavBar />
-      <Routes>
-        <Route index element={<MainPage />} />
-        <Route path="auth" element={<AuthLayout />}>
-          <Route index element={<Navigate to='signup' />}/>
-          <Route path='login' element={<LoginPage />} />
-          <Route path="signup" element={<SignUpPage />} />
-          <Route path="*" element={<Navigate to="/auth/signup" />} />
-        </Route>
-        <Route path="posts" element={
-          <ProtectedRoute redirectTo='/auth/login'>
-            <PostsLayout />
-          </ProtectedRoute>
-        }>
-          <Route index element={<PostsListPage />} />
-          <Route path=":postId" element={<PostPage />} />
-        </Route>
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-
+      {elements}
       <ToastContainer />
     </div>
   )
+  // другой вариант
+  // return (
+  //   <div className="min-h-screen bg-slate-50 dark:bg-slate-900 transition-colors duration-150 flex flex-col">
+  //     <NavBar />
+  //     <Routes>
+  //       <Route index element={<MainPage />} />
+  //       <Route path="auth" element={<AuthLayout />}>
+  //         <Route index element={<Navigate to='signup' />}/>
+  //         <Route path='login' element={<LoginPage />} />
+  //         <Route path="signup" element={<SignUpPage />} />
+  //         <Route path="*" element={<Navigate to="/auth/signup" />} />
+  //       </Route>
+  //       <Route path="posts" element={
+  //         <ProtectedRoute redirectTo='/auth/login' element={<PostsLayout />}/>
+  //       }>
+  //         <Route index element={<PostsListPage />} />
+  //         <Route path=":postId" element={<PostPage />} />
+  //       </Route>
+  //       <Route path="*" element={<Navigate to="/" />} />
+  //     </Routes>
+
+  //     <ToastContainer />
+  //   </div>
+  // )
 }
 
 const AppWithStoreAndRoutes = withRedux(withRouter(App))
--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import { Navigate, useLocation } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { isLoggedInSelector } from '../store/authSlice'
-function ProtectedRoute({ children, redirectTo = '/auth/login' }) {
-  const isLoggedIn = useSelector(isLoggedInSelector())
-  const location = useLocation()
+// import React from 'react'
+// import { Navigate, useLocation } from 'react-router-dom'
+// import { useSelector } from 'react-redux'
+// import { isLoggedInSelector } from '../store/authSlice'
+// function ProtectedRoute({ element, redirectTo = '/auth/login' }) {
+//   const isLoggedIn = useSelector(isLoggedInSelector())
+//   const location = useLocation()
 
-  return isLoggedIn ? children : <Navigate to={redirectTo} state={{referrer: location}}/>
-}
-export default ProtectedRoute
+//   return isLoggedIn ? element : <Navigate to={redirectTo} state={{referrer: location}}/>
+// }
+// export default ProtectedRoute
