c454ffa7e94af3c9bb048151ccaa089307134630
ZagSerSan <yan.steys1997@gmail.com>
2023-09-02 11:07:51 +0200

    2. Модуль OS

---
+++ refs/os.js
@@ -0,0 +1,12 @@
+// standart module Operation System -> os
+const os = require('os')
+
+// console.log(os.platform())
+// console.log(os.arch())
+// console.log(os.cpus())
+// console.log(os.freemem())
+// console.log(os.totalmem())
+// console.log(os.uptime()/60/60)
+console.log(os.homedir())
+
+
