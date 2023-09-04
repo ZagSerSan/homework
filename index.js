85afa2531f0f260b1e00dfb9988daec286682f1c
ZagSerSan <yan.steys1997@gmail.com>
2023-09-04 09:18:40 +0200

    2. Автоматизация с Nodemon

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const chalk = require('chalk')
 
 const port = 3000
 const server = http.createServer((req, res) => {
+  console.log('req.method', req.method)
+  console.log('req.url', req.url)
+
   console.log(chalk.green('server!'))
-  res.end('Hello, the is server!')
+  res.end('Hello, the is server!!!')
 })
 
 server.listen(port, () => {
