32b1702f15f903b5aff853f6ca5f47cc5d397721
ZagSerSan <yan.steys1997@gmail.com>
2023-09-04 10:11:09 +0200

    5. Работа с Express.js

--- index.js
+++ index.js
@@ -1,40 +1,60 @@
 const http = require('http')
+const express = require('express')
 const chalk = require('chalk')
 const fs = require('fs/promises')
 const path = require('path')
 const { addNote } = require('./notes.controller')
+const { log } = require('console')
 
 const port = 3000
 const basePath = path.join(__dirname, 'pages')
 
-const server = http.createServer( async (req, res) => {
-  if (req.method === 'GET') {
-    const content = await fs.readFile(path.join(basePath, 'index.html'))
-    // res.setHeader('Content-Type', 'text/html')
-    // res.setHeader('Content-Type', 'text/plain')
-    // res.writeHead(404)
-    res.writeHead(200, {
-      'Content-Type': 'text/html'
-    })
-    res.end(content)
-  } else if (req.method === 'POST') {
-    const body = []
-    res.writeHead(200, {
-      'Content-Type': 'text/plain: charset=utf-8'
-    })
+// const server = http.createServer( async (req, res) => {
+//   if (req.method === 'GET') {
+//     const content = await fs.readFile(path.join(basePath, 'index.html'))
+//     // res.setHeader('Content-Type', 'text/html')
+//     // res.setHeader('Content-Type', 'text/plain')
+//     // res.writeHead(404)
+//     res.writeHead(200, {
+//       'Content-Type': 'text/html'
+//     })
+//     res.end(content)
+//   } else if (req.method === 'POST') {
+//     const body = []
+//     res.writeHead(200, {
+//       'Content-Type': 'text/plain: charset=utf-8'
+//     })
 
-    req.on('data', data => {
-      body.push(Buffer.from(data))
-    })
-    req.on('end', () => {
-      const title = body.toString().split('=')[1].replaceAll('+', ' ')
-      addNote(title)
-      res.end(`Post success, title = ${title}`)
-    })
+//     req.on('data', data => {
+//       body.push(Buffer.from(data))
+//     })
+//     req.on('end', () => {
+//       const title = body.toString().split('=')[1].replaceAll('+', ' ')
+//       addNote(title)
+//       res.end(`Post success, title = ${title}`)
+//     })
 
-  }
+//   }
+// })
+
+// server.listen(port, () => {
+//   console.log(chalk.blue(`server has been started on port: ${port}...`))
+// })
+
+const app = express()
+app.use(express.urlencoded({
+  extended: true
+}))
+
+// get запрос
+app.get('/', (req, res) => {
+  res.sendFile(path.join(basePath, 'index.html'))
+})
+app.post('/', async (req, res) => {
+  await addNote(req.body.title)
+  res.sendFile(path.join(basePath, 'index.html'))
 })
 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(chalk.blue(`server has been started on port: ${port}...`))
 })
